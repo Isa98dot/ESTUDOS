39a8b92e210adb0b6eeaa1f7ef8c5f654b304ac1
Isa <107596892+Isa98dot@users.noreply.github.com>
2023-09-13 18:48:04 -0300

    Add files via upload

---
+++ desafio6.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadarada
+n = int(input('Digite um número: '))
+print('Número', n, '\n O dobro é:', n*2, '\n O triplo é:', n*3, '\n A raiz quadrada é:', n**2)
+
+print('Número {}, o dobro {}, o triplo {} e a raiz quadrada {}'.format(n, n*2, n*3, n**(1/2)))
