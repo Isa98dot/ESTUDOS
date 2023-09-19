c3837a1ad338e9ed90cecc37e8ec226140f32165
Isa <107596892+Isa98dot@users.noreply.github.com>
2023-09-19 18:23:53 -0300

    Add files via upload

---
+++ desafio8.py
@@ -0,0 +1,4 @@
+# escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
+n = float(input('Digite em metros: '))
+print('Metros:', n, '\nCentímetros:', n*100, '\nMilímetros:', n*1000)
+
