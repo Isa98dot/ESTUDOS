97dbe33b1c0d7a8145e9364bb27bc5cebdfa47fd
Isa <107596892+Isa98dot@users.noreply.github.com>
2023-09-20 18:30:48 -0300

    Add files via upload

---
+++ desafio9.py
@@ -0,0 +1,4 @@
+# faça um programa que leia um número inteiro e mostre na tela a sua tabuada
+n = int(input('Digite um número: '))
+print('5x1=', n, '\n5x2=', n*2, '\n5x3=', n*3, '\n5x4=', n*4, '\n5x5=', n*5, '\n5x6=', n*6, '\n5x7=', n*7, '\n5x8=',
+      n*8, '\n5x9=', n*9, '\n5x10=', n*10)
